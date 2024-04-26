52ff26d62cc2a3ae19d9b2b3e40000877e101ba0
JAZMIN_LOPEZ_MARTINEZ <a.21308051280495@cbtis128.edu.mx>
2024-04-26 17:51:00 +0000

    actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,142 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: "demo_app",
+      home: MyHomePage(title: "demo_app"),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
+class _MyHomePageState extends State<MyHomePage> {
+  String selectedFlor = "Ninguna";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: Column(
+        children: [
+          const SizedBox(height: 40),
+          Text("Flor seleccionada: $selectedFlor \n",
+              style: const TextStyle(fontSize: 27)),
+          const SizedBox(height: 30),
+          SizedBox(
+            width: 250,
+            child: RadioMenuButton(
+              value: "Lirio ",
+              groupValue: selectedFlor,
+              onChanged: (selectedValue) {
+                setState(() => selectedFlor = selectedValue!);
+              },
+              style: ButtonStyle(
+                  shape: MaterialStatePropertyAll(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  elevation: const MaterialStatePropertyAll(2),
+                  backgroundColor:
+                      const MaterialStatePropertyAll(Colors.white)),
+              child: Row(
+                children: [
+                  const SizedBox(width: 20),
+                  SizedBox(
+                    height: 60,
+                    child: Image.network(
+                        "https://raw.githubusercontent.com/JazLopezMartinez/Img_FlutterFlow_IOS_6J/main/UII_IMG/Png2.png"),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text("Lirio", style: const TextStyle(fontSize: 25)),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 30),
+          SizedBox(
+            width: 250,
+            child: RadioMenuButton(
+              value: "Narciso",
+              groupValue: selectedFlor,
+              onChanged: (selectedValue) {
+                setState(() => selectedFlor = selectedValue!);
+              },
+              style: ButtonStyle(
+                  shape: MaterialStatePropertyAll(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  elevation: const MaterialStatePropertyAll(2),
+                  backgroundColor:
+                      const MaterialStatePropertyAll(Colors.white)),
+              child: Row(
+                children: [
+                  const SizedBox(width: 20),
+                  SizedBox(
+                    height: 60,
+                    child: Image.network(
+                        "https://raw.githubusercontent.com/JazLopezMartinez/Img_FlutterFlow_IOS_6J/main/UII_IMG/Png3.png"),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text("Narciso", style: const TextStyle(fontSize: 25)),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 30),
+          SizedBox(
+            width: 250,
+            child: RadioMenuButton(
+              value: "Peonia ",
+              groupValue: selectedFlor,
+              onChanged: (selectedValue) {
+                setState(() => selectedFlor = selectedValue!);
+              },
+              style: ButtonStyle(
+                  shape: MaterialStatePropertyAll(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  elevation: const MaterialStatePropertyAll(2),
+                  backgroundColor:
+                      const MaterialStatePropertyAll(Colors.white)),
+              child: Row(
+                children: [
+                  const SizedBox(width: 20),
+                  SizedBox(
+                    height: 60,
+                    child: Image.network(
+                        "https://raw.githubusercontent.com/JazLopezMartinez/Img_FlutterFlow_IOS_6J/main/UII_IMG/Png4.png"),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text("Peonia", style: const TextStyle(fontSize: 25)),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
